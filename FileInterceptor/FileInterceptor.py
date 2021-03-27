a13c9787ddeffe9df61798a1dac6cf997887a30e
Paul St. Amour <paul35444@aol.com>
2021-03-27 13:12:36 -0500

    -removed unnecessary :

--- FileInterceptor/FileInterceptor.py
+++ FileInterceptor/FileInterceptor.py
@@ -3,7 +3,7 @@ import netfilterqueue
 import scapy.all as scapy
 
 def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload()):
+    scapy_packet = scapy.IP(packet.get_payload())
 #check packets RAW layer
     if scapy_packet.haslayer(scapy.RAW):
 #check packets destination port for 80 (http)
